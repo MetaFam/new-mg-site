dd29d5a8e0d481af1e4ef2e17f23561f60f1f60b
innkeeping <innkeeping@metagame.wtf>
2024-09-21 11:46:14 -0500

    Adjust margins on header icons 🧕

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,8 +4,8 @@ import X from '../icons/x';
 export default function Header() {
     return (
         <header className="flex justify-center top-0 w-full mr-2 fixed bg-opacity-30 bg-purple-800 z-10">
-            <a href="https://x.com/metafam" className='w-[3rem] h-[3rem]'><X className="fill-[#60dcc1] hover:fill-white"/></a>
-            <a href="https://github.com/MetaFam" className='w-[3rem] h-[3rem]'><GitHub className="fill-[#60dcc1] hover:fill-white"/></a>
+            <a href="https://x.com/metafam" className='w-[3rem] h-[3rem]'><X className="fill-[#60dcc1] hover:fill-white m-1"/></a>
+            <a href="https://github.com/MetaFam" className='w-[3rem] h-[3rem]'><GitHub className="fill-[#60dcc1] hover:fill-white m-1"/></a>
         </header>
     );
 };
